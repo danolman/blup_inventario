3082972bf2b34de2ec6c44b52c3d0bf0202e5d2b
danolman <townsendaniel@gmail.com>
2015-03-24 23:18:49 -0400

    multiples tablas
    creacion de multiples tablas en factoria (fichaFactory)

--- www/js/app/DescripcionController.js
+++ www/js/app/DescripcionController.js
@@ -1,41 +1,6 @@
 angular.module('AppControllers')
 
-.controller('DescripcionController', function($scope, $rootScope){
-
-	 	existeDB = localStorage.getItem('existeDB');
-	 	console.log(existeDB);
-        var db = window.openDatabase("fichaProductoDB", "1.0", "descripcion del producto", 200000);
-        if(existeDB == null){
-        	crearBD();
-        }
-    
-	     function crearBD(){
-	     	db.transaction(creaNueva, errorDB, successBD);    	
-	     }
-
-	     function creaNueva(tx) {
-	     	 tx.executeSql('DROP TABLE IF EXISTS descripcion');
-	     	 var sql = "CREATE TABLE IF NOT EXISTS descripcion("
-	     	 			+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-	     	 			+ "nombre VARCHAR(50) )";
-			tx.executeSql(sql);
-
-			tx.executeSql("INSERT INTO descripcion (id, nombre) VALUES (1, 'daniel')");
-			console.log('creando');
-	     }
-
-	     function errorDB(err) {
-	     	 alert("Error processing SQL: "+err.code);
-	     }
-
-	     function successBD(){
-	     	localStorage.existeDB = 1;
-	     	alert("success!");
-	     }
-
-	
-	 
-	
-	
+.controller('DescripcionController', function($scope, fichaFactory){
+		
 });
 
--- www/js/app/FichaFactory.js
+++ www/js/app/FichaFactory.js
@@ -2,24 +2,61 @@ angular.module("AppFactories", [])
 
 .factory("fichaFactory", function() {
 
-	 document.addEventListener("deviceready", onDeviceReady, false);
+	 //document.addEventListener("deviceready", function(){
 
-	 function onDeviceReady() {
-        var db = window.openDatabase("descripcion", "1.0", "descripcion del producto", 200000);
-        db.transaction(crearBD, errorDB, successBD);
-     }
+	 	localStorage.clear();
+	 	var existeDB = localStorage.getItem('existeDB');
+	 	console.log(existeDB);
+        var db = window.openDatabase("fichaProductoDB", "1.0", "descripcion del producto", 200000);
+        if(existeDB == null){
+        	db.transaction(creaNueva, errorDB, successBD);
+        }
 
-     function crearBD(tx){
-     	tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(100),codigo VARCHAR(50))');
-     }
+	     function creaNueva(tx) {
+	     	 tx.executeSql('DROP TABLE IF EXISTS ficha');
+	     	 tx.executeSql('DROP TABLE IF EXISTS propiedad');
+	     	 tx.executeSql('DROP TABLE IF EXISTS insumo');
+	     	 var sql_desdcripcion = "CREATE TABLE IF NOT EXISTS ficha("
+	     	 			+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+	     	 			+ "nombre VARCHAR(50),"
+	     	 			+ "descripcion TEXT,"
+	     	 			+ "codigo VARCHAR(100),"
+	     	 			+ "valorHoras VARCHAR(50),"
+	     	 			+ "imgagen VARCHAR(250)"
+	     	 			+")";
+			var sql_propiedad = "CREATE TABLE IF NOT EXISTS propiedad("
+	     	 			+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+	     	 			+ "idFicha NUMBER(50),"
+	     	 			+ "propiedad VARCHAR(250)"
+	     	 			+")";
+			var sql_insumo = "CREATE TABLE IF NOT EXISTS insumo("
+	     	 			+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+	     	 			+ "idFicha NUMBER(50),"
+	     	 			+ "tipo VARCHAR(100),"
+	     	 			+ "costo NUMBER(100),"
+	     	 			+ "medida VARCHAR(50),"
+	     	 			+ "cantidad NUMBER(100),"
+	     	 			+ "UnidadMedida VARCHAR(50)"
+	     	 			+")";
 
-     function errorDB(tx, err) {
-     	 alert("Error processing SQL: "+err);
-     }
+			tx.executeSql(sql_desdcripcion);
+			tx.executeSql(sql_propiedad);
+			tx.executeSql(sql_insumo);
 
-     function successBD(){
-     	alert("success!");
-     }
+			console.log('creando');
+	     }
+
+	     function errorDB(err) {
+	     	alert("Error processing SQL: "+err.code);
+	     }
+
+	     function successBD(){
+	     	localStorage.existeDB = 1;
+	     	console.log("success!");
+	     }	
+
+	 //}, false);
+
 		return{
 			//fichaProducto : fichaProducto
 		}
